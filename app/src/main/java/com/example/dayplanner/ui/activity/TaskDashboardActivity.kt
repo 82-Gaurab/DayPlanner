1a5d86799e1c5d29fe3a9bc101f3e6eb0a4feb7f
GAURAB <gaurabkhadka4082@gmail.com>
2025-01-22 18:12:38 +0545

    Intent from dashboard to add

--- app/src/main/java/com/example/dayplanner/ui/activity/TaskDashboardActivity.kt
+++ app/src/main/java/com/example/dayplanner/ui/activity/TaskDashboardActivity.kt
@@ -1,17 +1,30 @@
 package com.example.dayplanner.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.dayplanner.R
+import com.example.dayplanner.databinding.ActivityTaskDashboardBinding
 
 class TaskDashboardActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityTaskDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_task_dashboard)
+
+        binding = ActivityTaskDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addTaskFloatingBtn.setOnClickListener {
+            startActivity(Intent(this@TaskDashboardActivity, AddTaskActivity::class.java))
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.editTaskDesc)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
