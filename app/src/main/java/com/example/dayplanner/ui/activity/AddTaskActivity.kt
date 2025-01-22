c72fd25ef37eea0ed3705fde7ff0890892a229a8
GAURAB <gaurabkhadka4082@gmail.com>
2025-01-22 16:52:42 +0545

    Add add task activity

---
+++ app/src/main/java/com/example/dayplanner/ui/activity/AddTaskActivity.kt
@@ -0,0 +1,28 @@
+package com.example.dayplanner.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dayplanner.R
+import com.example.dayplanner.databinding.ActivityAddTaskBinding
+
+class AddTaskActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAddTaskBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.editTaskDesc)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
