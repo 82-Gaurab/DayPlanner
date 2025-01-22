f1a40412d9c113196cb653b56c9e73ca1dbe882e
GAURAB <gaurabkhadka4082@gmail.com>
2025-01-22 16:53:39 +0545

    Add dashord

---
+++ app/src/main/java/com/example/dayplanner/ui/activity/TaskDashboardActivity.kt
@@ -0,0 +1,21 @@
+package com.example.dayplanner.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dayplanner.R
+
+class TaskDashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_task_dashboard)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.editTaskDesc)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
