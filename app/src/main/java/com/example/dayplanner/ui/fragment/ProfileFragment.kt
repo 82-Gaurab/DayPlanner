8c129c4f9140dc5846fa1c10ee21190b022d0faa
GAURAB <gaurabkhadka4082@gmail.com>
2025-02-25 20:57:02 +0545

    Upgrade profile design; implement logout functionality

--- app/src/main/java/com/example/dayplanner/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/dayplanner/ui/fragment/ProfileFragment.kt
@@ -1,60 +1,78 @@
 package com.example.dayplanner.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.dayplanner.R
+import com.example.dayplanner.databinding.FragmentProfileBinding
+import com.example.dayplanner.model.UserModel
+import com.example.dayplanner.repository.UserRepositoryImp
+import com.example.dayplanner.ui.activity.AddTaskActivity
+import com.example.dayplanner.ui.activity.LoginActivity
+import com.example.dayplanner.viewmodel.UserViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var userViewModel : UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var repository = UserRepositoryImp()
+        userViewModel = UserViewModel(repository)
+
+        var currentUser =userViewModel.getCurrentUser()
+
+        currentUser.let {
+            userViewModel.getUserFromDatabase(it?.uid.toString())
+        }
+
+        userViewModel.userData.observe(requireActivity()) { user ->
+            binding.profileUsername.text =user?.firstName + " " + user?.lastName
+            binding.profileEmail.text =user?.email
+        }
+
+        binding.cardLogOut.setOnClickListener {
+            userViewModel.logout { success, message ->
+                if (success) {
+                    // Handle successful logout (e.g., navigate to login screen)
+                    Toast.makeText(context, "Logout successful", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(requireContext(), LoginActivity::class.java))
+                } else {
+                    // Handle logout failure (e.g., show error message)
+                    Toast.makeText(context, "Logout failed: $message", Toast.LENGTH_SHORT).show()
                 }
             }
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
