641c70b82328430a98682f71f45cc14155c96722
GAURAB <gaurabkhadka4082@gmail.com>
2025-02-27 08:58:53 +0545

    Create loading util

---
+++ app/src/main/java/com/example/dayplanner/utils/LoadingUtils.kt
@@ -0,0 +1,27 @@
+package com.example.dayplanner.utils
+
+import android.app.Activity
+import android.app.AlertDialog
+import com.example.dayplanner.R
+
+class LoadingUtils (val activity: Activity) {
+    lateinit var alertDialog: AlertDialog
+
+    fun show() {
+        val builder = AlertDialog.Builder(activity)
+
+        val dialogView = activity.layoutInflater.inflate(
+            R.layout.loading, null
+        )
+
+        builder.setView(dialogView)
+        builder.setCancelable(false)
+
+        alertDialog = builder.create()
+        alertDialog.show()
+    }
+
+    fun dismiss(){
+        alertDialog.dismiss()
+    }
+}
