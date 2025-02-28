ea1596700bcaf527ef223e11fe4246b63f928ec9
GAURAB <gaurabkhadka4082@gmail.com>
2025-02-28 15:47:37 +0545

    Make only main task runnable; Style main activity; Add funcitonality to main activity

--- app/src/main/java/com/example/dayplanner/MainActivity.kt
+++ app/src/main/java/com/example/dayplanner/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.dayplanner
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dayplanner.databinding.ActivityMainBinding
+import com.example.dayplanner.ui.activity.DashBoardActivity
+import com.example.dayplanner.ui.activity.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.enterBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.editTaskDesc)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
